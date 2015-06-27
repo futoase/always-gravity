c7c86c7227b4f3fa2af11a47f66876fb5c47c444
futoase <futoase@gmail.com>
2015-06-27 19:53:40 +0900

    Modified: Remove of velocity.js on myunit dir.

--- app/dest/game.js
+++ app/dest/game.js
@@ -1301,31 +1301,6 @@ playState.updateMyUnit = function () {
   myUnit.update();
 };
 
-playState.overTheLimitVelocityCount = function () {
-  var myUnit = MyUnit.instance;
-  var hud = HUD.instance;
-  myUnit.context = this;
-  hud.context = this;
-
-  if (this.overTheLimitVelocityCounter === undefined) {
-    this.overTheLimitVelocityCounter = 0;
-  }
-
-  if (hud.velocityBar.counter.current >= this.LIMIT_VELOCITY) {
-    if (this.contains(this.slowDownCountText)) {
-      this.slowDownCountText.text = this.LIMIT_VELOCITY_MAX_COUNT - this.overTheLimitVelocityCounter;
-    }
-    this.overTheLimitVelocityCounter++;
-  } else {
-    this.overTheLimitVelocityCounter = 0;
-    this.slowDownCountText.text = this.LIMIT_VELOCITY_MAX_COUNT;
-  }
-
-  if (this.overTheLimitVelocityCounter > this.LIMIT_VELOCITY_MAX_COUNT) {
-    myUnit.explosion();
-  }
-};
-
 playState.explosionRhombus = function (rhombus) {
   var rhombusSplinterMembers = this.rhombusSplinterPool.members;
   var angleBase = parseInt(360 / this.NUMBER_OF_RHOMBUS_SPLINTER);
