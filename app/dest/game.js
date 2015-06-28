537fc0481174913acd9a7a0e4fc6ecfce33e17db
futoase <futoase@gmail.com>
2015-06-28 13:04:07 +0900

    Modified: Removed myunit/speed-limit.js.

--- app/dest/game.js
+++ app/dest/game.js
@@ -1444,29 +1444,6 @@ playState.explosionOfMyUnit = function () {
   myUnit.explosion();
 };
 
-playState.speedLimitOfUnit = function () {
-  var hud = HUD.instance;
-  hud.context = this;
-
-  if (hud.velocityBar.counter.current >= this.LIMIT_VELOCITY * 0.95) {
-    this.soundEffectOfCautionForSpeed.play();
-    if (!this.contains(this.slowDownText)) {
-      this.addChild(this.slowDownText);
-    }
-    if (!this.contains(this.slowDownCountText)) {
-      this.addChild(this.slowDownCountText);
-    }
-  } else {
-    this.soundEffectOfCautionForSpeed.stop();
-    if (this.contains(this.slowDownText)) {
-      this.removeChild(this.slowDownText);
-    }
-    if (this.contains(this.slowDownCountText)) {
-      this.removeChild(this.slowDownCountText);
-    }
-  }
-};
-
 playState.updateMyUnit = function () {
   var myUnit = MyUnit.instance;
   myUnit.context = this;
