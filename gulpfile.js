52b03c33bd45a5b7969e327c855f09189181e291
futoase <futoase@gmail.com>
2015-07-11 14:17:29 +0900

    Modified: Be able to Always build a package for Always-Gravity!

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,32 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var electron = require('gulp-electron');
 var watch = require('gulp-watch');
 var babel = require('gulp-babel');
+var packageJson = require('./app/package.json');
+
+gulp.task('electron', function() {
+  gulp.src('')
+    .pipe(electron({
+      src: './app',
+      packageJson: packageJson,
+      release: './release',
+      cache: './cache',
+      version: 'v0.29.2',
+      packaging: true,
+      platforms: ['darwin-x64'],
+      platformResources: {
+        darwin: {
+          CFBundleDisplayName: packageJson.name,
+          CFBundleIdentifier: packageJson.name,
+          CFBundleName: packageJson.name,
+          CFBundleVersion: packageJson.version,
+          icon: 'always-gravity.icns'
+        }
+      }
+    }))
+    .pipe(gulp.dest(''));
+});
 
 gulp.task('concat', function(cb){
   return gulp.src([
