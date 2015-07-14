c141bfb1c2397921b41ca35db29791849d67fe76
futoase <futoase@gmail.com>
2015-07-15 01:48:23 +0900

    Created: Added of spec for GameOver.

---
+++ app/spec/model/game-over.spec.js
@@ -0,0 +1,15 @@
+describe('GameOver', () => {
+
+  beforeEach(() => {
+    GameState.current.boot();
+  });
+
+  describe('#execute', () => {
+    it ('should be running a method of execute.', () => {
+      GameOver.status = false;
+      GameOver.execute();
+      assert(GameOver.status === true);
+    });
+  });
+
+});
