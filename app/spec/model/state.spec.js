3f5973af24b2a64b7e090a2b0841da50134d09f0
futoase <futoase@gmail.com>
2015-07-15 22:37:01 +0900

    Created: Create new spec of GameState.

---
+++ app/spec/model/state.spec.js
@@ -0,0 +1,15 @@
+describe('GameState', () => {
+
+  describe('#instance', () => {
+    it ('should be return a instance of GameState.', () => {
+      assert(GameState.instance instanceof GameState);
+    });
+  });
+
+  describe('#current', () => {
+    it ('should be return a current of state for Game.', () => {
+      assert(GameState.current === testState);
+    });
+  });
+
+});
